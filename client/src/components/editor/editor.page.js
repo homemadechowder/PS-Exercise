f4ace2e30ddd166fd335b1050716a290a07eee04
Andrew Lu <andrew@cohome.com>
2021-02-05 16:58:58 -0800

    Drag somewhat done, can drag empty div to make empty, now to figure out how to swap

--- client/src/components/editor/editor.page.js
+++ client/src/components/editor/editor.page.js
@@ -5,31 +5,40 @@ import assign from '../../scripts/assign';
 
 import './editor.style.scss';
 
+const imageList = [
+    'https://static5.depositphotos.com/1000350/432/i/950/depositphotos_4327684-stock-photo-doctor-with-stethoscope-fixing-laptop.jpg',
+    'https://mk0knowtechie1qof48y.kinstacdn.com/wp-content/uploads/2019/11/dark-stock-photos-main-kt.jpg',
+    'https://pbs.twimg.com/media/EW8GhG_XkAEOyAh.jpg',
+    'https://www.fodors.com/wp-content/uploads/2020/04/BizarreStockPhotos__HERO_shutterstock_1308961057.jpg',
+    'https://www.adweek.com/files/2015_May/iStock-Unfinished-Business-6.jpg',
+    'https://st2.depositphotos.com/1912933/7560/i/950/depositphotos_75608753-stock-photo-angry-old-woman-with-an.jpg',
+]
+
+const defaultLayout = [
+    {
+        id: '0',
+    },
+    {
+        id: '1',
+    },
+    {
+        id: '2',
+    },
+    {
+        id: '3',
+    }
+]
+
+let layoutList = [];
+
 export default function Editor(){
-
-    const defaultLayout = [
-        {
-            id: '0',
-        },
-        {
-            id: '1',
-        },
-        {
-            id: '2',
-        },
-        {
-            id: '3',
-        }
-
-    ]
 
     const [config, setConfig] = useState();
     const [savedLayout, setSavedLayout] = useState([]);
     const [actionTarget, setActionTarget] = useState();
-    let layoutList = [];
-    const addTarget = (event) => {
-        console.log(event.target.id)
-        setActionTarget(event.target.id);   
+
+    const addTarget = (ev) => {
+        setActionTarget(ev.target.id);   
     }
 
     const allowDrop = (ev) => {
@@ -38,34 +47,64 @@ export default function Editor(){
       
     const drag = (ev) => {
         ev.dataTransfer.setData("text", ev.target.id);
+        
+        const source = ev.target.id ? document.getElementById(ev.target.id).parentNode.id : '';
+        ev.dataTransfer.setData("source", source);
     }
       
     const drop = (ev) => {
         ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        var img = document.createElement('img'); 
-        img.style=`background-image: url(${data})`
-        img.classList.add('editor__drag-image')
-        ev.target.appendChild(img);
+
+        const source = ev.dataTransfer.getData("source");
+        const sourceDiv = document.getElementById(source);
+        console.log('source div >>>', sourceDiv)
+        const targetDiv = document.getElementById(ev.target.id);
+        console.log('target div >>>', targetDiv)
+
+        if (targetDiv.childNodes.length !== 0){
+            targetDiv.removeChild(targetDiv.childNodes[0])
+        }
+
+        if (sourceDiv !== null){
+
+            const sourceImg = sourceDiv.childNodes[0].childNodes[0] || null;
+
+            sourceImg ? console.log('source img >>>', sourceImg.style.backgroundImage) : console.log('its empty div')
+            console.log('target Div >>>', targetDiv.innerHTML);
+            if (sourceImg !== null){
+                const background = sourceImg.style.backgroundImage
+                var imgDiv = document.createElement('img')
+                imgDiv.style=`background-image: ${background}`
+                imgDiv.classList.add('editor__drag-image')
+                ev.target.appendChild(imgDiv)
+            }
+
+        } else {
+            var data = ev.dataTransfer.getData("text");
+            var img = document.createElement('img'); 
+            img.style=`background-image: url(${data})`
+            img.classList.add('editor__drag-image')
+            ev.target.appendChild(img);
+        }
+        
     }
 
     const handleFirstSplit = (split) =>{        
         if (split.mode){
         return(
             <Grid container id={split.id} direction={split.mode==='vertical'?'row':'column'} key={`box${split.id}`} className='editor__box-row'>
-                <Grid onDrop={(e)=>drop(e)} onDragOver={(e)=>allowDrop(e)} container direction={split.mode==='vertical'?'row':'column'} tabindex='0' item xs id={split.split0?split.split0.id:split.id+'0'} style={{border: '0.1px solid lightgrey'}} onClick={(e)=>addTarget(e)}>
+                <Grid onDrop={(e)=>drop(e)} onDragOver={(e)=>allowDrop(e)} draggable="true" onDragStart={(e)=>drag(e)} container direction={split.mode==='vertical'?'row':'column'} tabindex='0' item xs id={split.split0?split.split0.id:split.id+'0'} style={{border: '0.1px solid lightgrey'}} onClick={(e)=>addTarget(e)}>
                     {handleSplit0(split.split0)}
                 </Grid>   
-                <Grid onDrop={(e)=>drop(e)} onDragOver={(e)=>allowDrop(e)} container direction={split.mode==='vertical'?'row':'column'} tabindex='0' item xs id={split.split1?split.split1.id:split.id+'1'} style={{border: '0.1px solid lightgrey'}} onClick={(e)=>addTarget(e)}>        
+                <Grid onDrop={(e)=>drop(e)} onDragOver={(e)=>allowDrop(e)} draggable="true" onDragStart={(e)=>drag(e)} container direction={split.mode==='vertical'?'row':'column'} tabindex='0' item xs id={split.split1?split.split1.id:split.id+'1'} style={{border: '0.1px solid lightgrey'}} onClick={(e)=>addTarget(e)}>        
                     {handleSplit1(split.split1)}
-                </Grid>
-               
+                </Grid>   
             </Grid>
         )   
         }
         return(
             <Grid container id={split.id} direction={split.mode==='vertical'?'row':'column'} key={`box${split.id}`} className='editor__box-row'>
-                <Grid onDrop={(e)=>drop(e)} onDragOver={(e)=>allowDrop(e)} container direction={split.mode==='vertical'?'row':'column'} tabindex='0' item xs id={split.split0?split.split0.id:split.id+'0'} style={{border: '0.1px solid lightgrey'}} onClick={(e)=>addTarget(e)}>
+                <Grid onDrop={(e)=>drop(e)} onDragOver={(e)=>allowDrop(e)} draggable="true" onDragStart={(e)=>drag(e)} container direction={split.mode==='vertical'?'row':'column'} tabindex='0' item xs id={split.split0?split.split0.id:split.id+'0'} style={{border: '0.1px solid lightgrey'}} onClick={(e)=>addTarget(e)}>
                 </Grid>
             </Grid>
         ) 
@@ -118,7 +157,6 @@ export default function Editor(){
         targetDiv.removeChild(targetDiv.childNodes[0])
         }
 
-        console.log(actionTarget);
         for (var i = 1; i < actionTarget.length; i++){
            if (actionTarget[i] === '0'){
                keyPath.push('split0')
@@ -145,10 +183,11 @@ export default function Editor(){
 
     const saveLayout = (currentConfig) => {
         // honestly does not make any sense to me but this will do for now
-        layoutList = [...JSON.parse(JSON.stringify(savedLayout))];
+        layoutList = JSON.parse(JSON.stringify([...savedLayout]));
         layoutList.push(JSON.parse(JSON.stringify(currentConfig)));    
         alert(`Saved as: Save Item ${layoutList.length}`)
-        setSavedLayout(layoutList);
+        console.log(layoutList);
+        setSavedLayout(JSON.parse(JSON.stringify(layoutList)));
     }
 
     const handleSavedLayout = () => {
@@ -193,42 +232,22 @@ export default function Editor(){
 
             
             {config &&
-            <Grid container>
-                <Grid item xs>
+            <Grid container justify="center">
+                <Grid item xs={4}>
                     <section className='editor__image-section'> 
+                    {imageList.map((image)=>{
+                        return(
                         <img 
                             className='editor__pallet-image' 
-                            src = 'https://static5.depositphotos.com/1000350/432/i/950/depositphotos_4327684-stock-photo-doctor-with-stethoscope-fixing-laptop.jpg'
+                            src = {image}
+                            key = {image}
                             draggable="true" onDragStart={(e)=>drag(e)}
                         ></img>
-                        <img 
-                            className='editor__pallet-image' 
-                            src = 'https://mk0knowtechie1qof48y.kinstacdn.com/wp-content/uploads/2019/11/dark-stock-photos-main-kt.jpg'
-                            draggable="true" onDragStart={(e)=>drag(e)}
-                        ></img>
-                        <img 
-                            className='editor__pallet-image' 
-                            src = 'https://pbs.twimg.com/media/EW8GhG_XkAEOyAh.jpg'
-                            draggable="true" onDragStart={(e)=>drag(e)}
-                        ></img>
-                        <img 
-                            className='editor__pallet-image' 
-                            src = 'https://www.fodors.com/wp-content/uploads/2020/04/BizarreStockPhotos__HERO_shutterstock_1308961057.jpg'
-                            draggable="true" onDragStart={(e)=>drag(e)}
-                        ></img>
-                        <img 
-                            className='editor__pallet-image' 
-                            src = 'https://www.adweek.com/files/2015_May/iStock-Unfinished-Business-6.jpg'
-                            draggable="true" onDragStart={(e)=>drag(e)}
-                        ></img>
-                         <img 
-                            className='editor__pallet-image' 
-                            src = 'https://st2.depositphotos.com/1912933/7560/i/950/depositphotos_75608753-stock-photo-angry-old-woman-with-an.jpg'
-                            draggable="true" onDragStart={(e)=>drag(e)}
-                        ></img>
+                        )
+                    })}
                     </section>
                 </Grid>
-            <Grid item xs>
+            <Grid item xs={5}>
                 <section className='editor__box-section'>
                     {config.map((box)=>{
                         return(      
